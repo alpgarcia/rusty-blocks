a1be0a756d84cd8cd0d3585b863042a02ea4090b
Alberto Pérez García-Plaza <3502941+alpgarcia@users.noreply.github.com>
2025-04-09 23:50:57 +0200

    Add basic shape drop system
    * Implements quick drop (down arrow)
    * Fix shapes to the playfield when reach the bottom edge
    * Random shapes spawns after a piece is fixed
    * TODOs for next steps

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,8 @@ const SHIFT_DELAY: f64 = 0.1;
 fn generate_srs_shapes() -> Vec<Box<dyn Rotation>> {
 
     // TODO move this to shapes module?
+    // TODO: separate SRS and NES shapes
+
     let j: Vec<usize> =Vec::from([
         1, 0, 0,
         1, 1, 1,
@@ -110,13 +112,13 @@ fn generate_srs_shapes() -> Vec<Box<dyn Rotation>> {
         Box::new(StillShape::build(ShapeData::build(o, 4, YELLOW))),
 
         // NES
-        Box::new(Shape::build(ShapeData::build(j_nes, 3, PINK))),
-        Box::new(Shape::build(ShapeData::build(l_nes, 3, BLUE))),
-        Box::new(NesShape::build(ShapeData::build(s_nes, 3, GREEN))),
-        Box::new(NesShape::build(ShapeData::build(z_nes, 3, ORANGE))),
-        Box::new(NesShape::build(ShapeData::build(i_nes, 4, RED))),
-        Box::new(Shape::build(ShapeData::build(t_nes, 3, PURPLE))),
-        Box::new(StillShape::build(ShapeData::build(o_nes, 4, YELLOW))),
+        // Box::new(Shape::build(ShapeData::build(j_nes, 3, PINK))),
+        // Box::new(Shape::build(ShapeData::build(l_nes, 3, BLUE))),
+        // Box::new(NesShape::build(ShapeData::build(s_nes, 3, GREEN))),
+        // Box::new(NesShape::build(ShapeData::build(z_nes, 3, ORANGE))),
+        // Box::new(NesShape::build(ShapeData::build(i_nes, 4, RED))),
+        // Box::new(Shape::build(ShapeData::build(t_nes, 3, PURPLE))),
+        // Box::new(StillShape::build(ShapeData::build(o_nes, 4, YELLOW))),
     ]
 }
 
@@ -177,15 +179,16 @@ fn draw_shape(shape: &dyn Rotation, pos_x: f32, pos_y: f32,
                 block_size - 2.0,
                 shape.shape_data().color(),
             );
-        } else {
-            draw_rectangle(
-                pos_x + (shape_col as f32 * block_size) + 1.0,
-                pos_y + (shape_row as f32 * block_size) + 1.0,
-                block_size - 2.0,
-                block_size - 2.0,
-                BLACK,
-            );
-        }
+        } 
+        // else {
+        //     draw_rectangle(
+        //         pos_x + (shape_col as f32 * block_size) + 1.0,
+        //         pos_y + (shape_row as f32 * block_size) + 1.0,
+        //         block_size - 2.0,
+        //         block_size - 2.0,
+        //         BLACK,
+        //     );
+        // }
     }
 
 }
@@ -217,25 +220,47 @@ fn rotate_counter_cw(pf: &Playfield, shape: &dyn Rotation,
     }
 }
 
+fn random_shape(shapes: &Vec<Box<dyn Rotation>>) -> &Box<dyn Rotation> {
+    
+    // TODO: implement different shape generation algorithms
+
+    let shape_idx = rand::gen_range(0, shapes.len());
+    
+    &shapes[shape_idx]
+
+}
+
 #[macroquad::main("Rusty Blocks")]
 async fn main() {
+
+    // TODO: add a menu
+
+    // Initialize random number generator
+    rand::srand(miniquad::date::now() as u64);
 
     // TODO try to add NES & Gameboy shapes options too
     let shapes = generate_srs_shapes();
 
-    let pf = Playfield::build();
-
-    let current_shape = &shapes[0];
+    let mut pf = Playfield::build();
 
     let mut rotation_demo = false;
+
+    // Initialize first shape
+    let mut current_shape = random_shape(&shapes);
     let mut cs_col = (pf.n_cols() / 2) 
                      - (current_shape.shape_data().width() / 2);
-    let cs_row = 0;
-    // let mut col_f = 0.0; 
+    let mut cs_row: usize = 0;
     let mut rot: isize = 0;
+    let mut spawn_shape = false;
 
-    let mut start = get_time();
+    // Initialize Shape Drop System
+    let mut drop_start = get_time();
+    let drop_delay = 0.8;
+
+    // Initialize Shift Delay System
+    let mut shift_start = get_time();
     let mut first_press = true;
+    
     loop {
 
         // let delta = get_frame_time();
@@ -254,6 +279,30 @@ async fn main() {
                     - ((pf.n_cols() / 2) as f32 * block_size);
         let pf_y = (screen_height() / 2.0) 
                     - ((pf.n_rows() / 2) as f32 * block_size);
+
+        // SPAWN NEW SHAPE --------------------------------------------
+            
+        if spawn_shape {
+            
+            // TODO: shape parameters could be stored in a struct
+            // TODO: this code is repeated before starting the loop
+            // TODO: add a queue of shapes to spawn
+            current_shape = random_shape(&shapes);
+
+            cs_col = (pf.n_cols() / 2) 
+                     - (current_shape.shape_data().width() / 2);
+            cs_row = 0;
+            rot = 0;
+            spawn_shape = false;
+
+            drop_start = get_time();
+
+            if pf.collides(&**current_shape, cs_row, cs_col as isize, rot) {
+                // TODO game over
+                println!("Game Over");
+                break;
+            }
+        }
 
         // PROCESS INPUT ----------------------------------------------
 
@@ -290,7 +339,8 @@ async fn main() {
         }
 
         if is_key_released(KeyCode::Left) || 
-            is_key_released(KeyCode::Right) {
+            is_key_released(KeyCode::Right) ||
+            is_key_released(KeyCode::Down) {
             first_press = true;
         }
             
@@ -298,10 +348,11 @@ async fn main() {
             && !pf.collides(
                 &**current_shape, cs_row, cs_col as isize - 1, rot) {
                 
-                if first_press || get_time() - start >= SHIFT_DELAY {
+                if first_press || get_time() - shift_start >= SHIFT_DELAY {
+                    
                     cs_col -= 1;
                     first_press = false;
-                    start = get_time();
+                    shift_start = get_time();
                 }
         }
 
@@ -309,13 +360,57 @@ async fn main() {
             && !pf.collides(
                 &**current_shape, cs_row, cs_col  as isize + 1 as isize, rot) {
                 
-                if first_press || get_time() - start >= SHIFT_DELAY {
+                // TODO: apply a different delay for quick drop
+                if first_press || get_time() - shift_start >= SHIFT_DELAY {
+                    
                     cs_col += 1;
                     first_press = false;
-                    start = get_time();
+                    shift_start = get_time();
                 }
         }
-        
+
+        if is_key_down(KeyCode::Down) {
+            if !pf.collides(
+                &**current_shape, cs_row + 1, cs_col as isize, rot) {
+                
+                if first_press || get_time() - shift_start >= SHIFT_DELAY {
+                    
+                    cs_row += 1;
+                    first_press = false;
+                    shift_start = get_time();
+                    // Cancel drop delay
+                    drop_start = get_time();
+                }
+
+            } else {
+                spawn_shape = true;
+            }
+        }
+
+        // TODO: implement hard drop
+
+        // PROCESS FALLING RATE ---------------------------------------
+
+        // We do this after processing input to ensure that the
+        // the shape is fixed to the playfield in the correct position
+
+        if !spawn_shape && get_time() - drop_start >= drop_delay {
+            if !pf.collides(
+                &**current_shape, cs_row + 1, cs_col as isize, rot) {
+                
+                cs_row += 1;
+                drop_start = get_time();
+                
+            } else {
+                spawn_shape = true;
+            }
+        }
+
+        // FIX SHAPE --------------------------------------------------
+
+        if spawn_shape {
+            pf.add(&**current_shape, cs_row, cs_col, rot);
+        }
 
         // DRAW PLAYFIELD ---------------------------------------------
 
